9c063a8087900d7faf05ccb5f44b13e3ee87dc8b
thulasigovardhan <thulasigovardhan@gmail.com>
2022-01-24 19:30:25 -0500

    Demonstrating usage of css module to a single component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,38 +1,40 @@
-import styled from 'styled-components';
+import classes from './Button.module.css';
 
-const Button = styled.button`
-  width: 100%;
-  font: inherit;     //Class or ID name should not be given instead give
-  padding: 0.5rem 1.5rem;    //the css styles directly
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// import styled from 'styled-components';
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;     //Class or ID name should not be given instead give
+//   padding: 0.5rem 1.5rem;    //the css styles directly
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-&:focus {  //for Psedo selectors like this instead of giving the tagname(.button)
-  outline: none; //you should give '&' instead
-}
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
-`;
+// &:focus {  //for Psedo selectors like this instead of giving the tagname(.button)
+//   outline: none; //you should give '&' instead
+// }
+
+// &:hover,
+// &:active {
+//   background: #ac0e77;
+//   border-color: #ac0e77;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }
+// `;
 
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={classes.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
